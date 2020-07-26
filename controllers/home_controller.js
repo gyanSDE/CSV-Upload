a4ad670f65a5e3a2a9144a5b914bf7eeab910262
Gyan <gyankr20@gmail.com>
2020-07-26 21:04:05 +0530

    Install ejs and create form

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
-module.exports.home=function(req,res){
-    return res.end('<h1> Express is up For iconnect </h1><h5>controller is loaded</h5>');
+module.exports.home=(req,res)=>{
+    return res.render('home',{
+        title: "Home"
+    });
 };
