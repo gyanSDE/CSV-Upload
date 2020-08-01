897b4bab6aab998e0dee6641b2cdc5916b55977d
Gyan <gyankr20@gmail.com>
2020-08-01 11:33:31 +0530

    Display Csv On UI

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,15 @@
 const Csv = require("../models/csvFile");
+const path = require('path');
+const csvtojson = require("csvtojson");
 
 module.exports.home = async (req, res) => {
 
     let csv = await Csv.find({})
 
-        return res.render('home',{
-            title: "CSV | Upload",
-            details: csv
-        });
+    return res.render('home', {
+        title: "CSV | Upload",
+        details: csv
+    });
 };
 module.exports.uploadCsv = async (req, res) => {
     await Csv.uploadedCsv(req, res, async function (err) {
@@ -22,5 +24,38 @@ module.exports.uploadCsv = async (req, res) => {
             });
         }
     })
+    return res.redirect('/');
+};
+
+module.exports.viewCsv = async (req, res) => {
+    let data = await Csv.findById(req.params.id)
+    const csvFilePath = path.join(__dirname, '..', data.fileUrl);
+    //const csv=require('csvtojson')
+    //csv()
+    //.fromFile(csvFilePath)
+    //.then((jsonObj)=>{
+    //console.log(jsonObj);
+    //})
+    const jsonArray = await csvtojson().fromFile(csvFilePath);
+
+    //console.log(jsonArray);
+    //console.log(jsonArray.length);
+
+    var keys = [];
+    for (let k in jsonArray[0]) {
+      keys.push(k);
+      
+    }
+
+   // console.log(keys);
+
+    return res.render('viewCsv', {
+        title: "CSV | View",
+        details: jsonArray,
+        keys:keys,
+        fileName:data.fileName
+    });
+
+
     return res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/',homeController.home);
 router.post('/upload-csv' , homeController.uploadCsv);
-
+router.get("/view-csv/:id", homeController.viewCsv);
 module.exports = router;
